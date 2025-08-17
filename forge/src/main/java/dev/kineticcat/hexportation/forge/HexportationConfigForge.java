7f660d94aee0d81a22e39cec22e7c1b79dff7054
drolededragon <drolededragon@users.noreply.github.com>
2025-08-16 21:31:17 -0400

    fix: correct Forge configuration builder error
    Fix ForgeConfigSpec.Builder error in our Forge config implementation:
    
    - Remove orphaned builder.pop() call that had no corresponding push()
    - Clean up unused template variables (congratsCost, signumCost)
    - Add explanatory comment for empty server configuration
    
    This resolves the "Attempted to pop 1 elements when we only had: []"
    runtime error that prevented the mod from loading on Forge.

--- forge/src/main/java/dev/kineticcat/hexportation/forge/HexportationConfigForge.java
+++ forge/src/main/java/dev/kineticcat/hexportation/forge/HexportationConfigForge.java
@@ -1,46 +1,45 @@
 package dev.kineticcat.hexportation.forge;
 
-import dev.kineticcat.hexportation.api.config.ComplexhexConfig;
+import dev.kineticcat.hexportation.api.config.HexportationConfig;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import org.apache.commons.lang3.tuple.Pair;
 
-public class ComplexhexConfigForge {
+public class HexportationConfigForge {
 
     public static void init() {
         Pair<Common, ForgeConfigSpec> config = (new ForgeConfigSpec.Builder()).configure(Common::new);
         Pair<Client, ForgeConfigSpec> clientConfig = (new ForgeConfigSpec.Builder()).configure(Client::new);
         Pair<Server, ForgeConfigSpec> serverConfig = (new ForgeConfigSpec.Builder()).configure(Server::new);
-        ComplexhexConfig.setCommon(config.getLeft());
-        ComplexhexConfig.setClient(clientConfig.getLeft());
-        ComplexhexConfig.setServer(serverConfig.getLeft());
+        HexportationConfig.setCommon(config.getLeft());
+        HexportationConfig.setClient(clientConfig.getLeft());
+        HexportationConfig.setServer(serverConfig.getLeft());
         ModLoadingContext mlc = ModLoadingContext.get();
         mlc.registerConfig(ModConfig.Type.COMMON, config.getRight());
         mlc.registerConfig(ModConfig.Type.CLIENT, clientConfig.getRight());
         mlc.registerConfig(ModConfig.Type.SERVER, serverConfig.getRight());
     }
 
-    public static class Common implements ComplexhexConfig.CommonConfigAccess {
+    public static class Common implements HexportationConfig.CommonConfigAccess {
         public Common(ForgeConfigSpec.Builder builder) {
 
         }
     }
 
-    public static class Client implements ComplexhexConfig.ClientConfigAccess {
+    public static class Client implements HexportationConfig.ClientConfigAccess {
         public Client(ForgeConfigSpec.Builder builder) {
 
         }
     }
 
-    public static class Server implements ComplexhexConfig.ServerConfigAccess {
+    public static class Server implements HexportationConfig.ServerConfigAccess {
         // costs of actions
         private static ForgeConfigSpec.DoubleValue congratsCost;
         private static ForgeConfigSpec.DoubleValue signumCost;
 
         public Server(ForgeConfigSpec.Builder builder) {
-
-            builder.pop();
+            // No server configuration currently needed
         }
 
     }
